e856f319749305bdd529f909e0e07bddf53cba7a
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-10 16:22:48 +0100

    add coverage

--- libs/budget-domain/jest.config.ts
+++ libs/budget-domain/jest.config.ts
@@ -9,4 +9,15 @@ export default {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/budget-domain',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
 };

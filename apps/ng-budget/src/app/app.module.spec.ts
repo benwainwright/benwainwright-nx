2a39c53e19a963527579e58c5eaf3f97f3d681a9
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-11 14:20:36 +0100

    formatted]

--- apps/ng-budget/src/app/app.module.spec.ts
+++ apps/ng-budget/src/app/app.module.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed } from '@angular/core/testing'
-import { AppModule } from './app.module'
-import { APP_BASE_HREF } from '@angular/common'
+import { TestBed } from '@angular/core/testing';
+import { AppModule } from './app.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('The app module', () => {
-    it('should render without error', async () => {
-        TestBed.configureTestingModule({
-            imports: [AppModule],
-            providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
-        })
+  it('should render without error', async () => {
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    });
 
-        const app = TestBed.inject(AppModule)
-        expect(app).toBeTruthy()
-    })
-})
+    const app = TestBed.inject(AppModule);
+    expect(app).toBeTruthy();
+  });
+});
--- apps/ng-budget/src/lib/uuid.ts
+++ apps/ng-budget/src/lib/uuid.ts
@@ -1,7 +1,7 @@
 export const uuid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
-        var random = (Math.random() * 16) | 0,
-            replaceString = char == 'x' ? random : (random & 0x3) | 0x8
-        return replaceString.toString(16)
-    })
-}
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
+    var random = (Math.random() * 16) | 0,
+      replaceString = char == 'x' ? random : (random & 0x3) | 0x8;
+    return replaceString.toString(16);
+  });
+};

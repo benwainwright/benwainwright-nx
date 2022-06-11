52b29ec7f373ced6b52b384700f9e2b657593839
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-11 01:11:59 +0100

    stuff

--- apps/ng-budget/src/app/settings/settings.component.ts
+++ apps/ng-budget/src/app/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { lastValueFrom, Subscription, take } from 'rxjs';
 import { SettingsService } from '../services/settings.service';
 
 @Component({
@@ -8,7 +8,7 @@ import { SettingsService } from '../services/settings.service';
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
 })
-export class SettingsComponent implements OnDestroy {
+export class SettingsComponent implements OnInit, OnDestroy {
   private subscription: Subscription | undefined;
 
   public form = new FormGroup({
@@ -19,8 +19,21 @@ export class SettingsComponent implements OnDestroy {
 
   constructor(private settingService: SettingsService) {
     this.subscription = this.form.valueChanges.subscribe((value) =>
-      this.settingService.setSettings(value)
+      this.settingService.setSettings({
+        payAmount: value.expectedSalary,
+        overdraft: value.overdraft,
+        payCycle: value.payCycle,
+      })
     );
+  }
+  async ngOnInit() {
+    const settings = await lastValueFrom(
+      this.settingService.getSettings().pipe(take(1))
+    );
+
+    this.form.controls.expectedSalary.setValue(settings.payAmount);
+    this.form.controls.overdraft.setValue(settings.overdraft);
+    this.form.controls.payCycle.setValue(settings.payCycle);
   }
 
   public ngOnDestroy(): void {

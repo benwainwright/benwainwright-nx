d3ffdfb70ebe68e13ca7f732e84736250d80a871
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-12 14:03:05 +0100

    added dialog

--- apps/ng-budget/src/app/create-payment-dialog/create-payment-dialog.component.ts
+++ apps/ng-budget/src/app/create-payment-dialog/create-payment-dialog.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'benwainwright-create-payment-dialog',
   templateUrl: './create-payment-dialog.component.html',
   styleUrls: ['./create-payment-dialog.component.css'],
 })
-export class CreatePaymentDialogComponent {}
+export class CreatePaymentDialogComponent {
+  public form = new FormGroup({
+    name: new FormControl<string>('', { nonNullable: true }),
+    amount: new FormControl<number>(0, { nonNullable: true }),
+    when: new FormControl<string>('', { nonNullable: true }),
+  });
+}

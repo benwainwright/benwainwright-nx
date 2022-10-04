12ef9c35472b51973dc6c5d9e74be9fd3641e47c
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-10-04 17:45:51 +0100

    build: add hosted UI stack config

--- apps/quick-budget-cdk/src/stacks/app-stack.ts
+++ apps/quick-budget-cdk/src/stacks/app-stack.ts
@@ -4,27 +4,31 @@ import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import {
+  CloudFrontTarget,
+  UserPoolDomainTarget,
+} from 'aws-cdk-lib/aws-route53-targets';
 
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import * as path from "node:path"
+import * as path from 'node:path';
 import { Environment } from './environment';
 import { getDomainName } from './get-domain-name';
 
-const PACKAGE_DIR = path.join(__dirname, '..', '..')
-const ROOT_DIR = path.join(PACKAGE_DIR, '..', '..')
-const DIST_DIR = path.join(ROOT_DIR, 'dist')
-const DEPLOYMENT_DIR = path.join(DIST_DIR, 'apps', 'ng-budget')
+const PACKAGE_DIR = path.join(__dirname, '..', '..');
+const ROOT_DIR = path.join(PACKAGE_DIR, '..', '..');
+const DIST_DIR = path.join(ROOT_DIR, 'dist');
+const DEPLOYMENT_DIR = path.join(DIST_DIR, 'apps', 'ng-budget');
 
 interface AppStackProps {
   transient: boolean;
-  envName: Environment
+  envName: Environment;
 }
 
 interface BackendConfig {
-  userpoolId: string
-  userPoolClientId: string
+  userpoolId: string;
+  userPoolClientId: string;
+  authSignInUrl: string;
 }
 
 export class AppStack extends Stack {
@@ -33,7 +37,7 @@ export class AppStack extends Stack {
 
     const removalPolicy = props.transient
       ? RemovalPolicy.DESTROY
-      : RemovalPolicy.RETAIN
+      : RemovalPolicy.RETAIN;
 
     const pool = new UserPool(this, 'user-pool', {
       removalPolicy,
@@ -57,33 +61,54 @@ export class AppStack extends Stack {
         username: true,
         email: true,
       },
-    })
+    });
+
+    const domainName = getDomainName(props.envName);
+
+    const hostedZone = HostedZone.fromLookup(this, 'hosted-zone', {
+      domainName,
+    });
+
+    const certificate = new DnsValidatedCertificate(
+      this,
+      'website-certificate',
+      {
+        region: 'us-east-1',
+        hostedZone,
+        domainName: `*.${domainName}`,
+      }
+    );
+
+    const userPoolDomain = pool.addDomain('user-pool-domain', {
+      customDomain: {
+        domainName: `auth.${domainName}`,
+        certificate,
+      },
+    });
+
+    new ARecord(this, 'a-record-for-cognito', {
+      recordName: `auth.${domainName}`,
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new UserPoolDomainTarget(userPoolDomain)),
+    });
 
     const client = new UserPoolClient(this, 'pool-client', {
-      userPool: pool
-    })
-    
-    const domainName = getDomainName(props.envName)
+      userPool: pool,
+      oAuth: {
+        flows: {
+          implicitCodeGrant: true,
+        },
+        callbackUrls: [`https://${domainName}/`],
+      },
+    });
 
     const assetsBucket = new Bucket(this, 'assets-bucket', {
       bucketName: domainName,
       publicReadAccess: true,
       websiteErrorDocument: 'index.html',
       websiteIndexDocument: 'index.html',
-      removalPolicy
+      removalPolicy,
     });
-
-    const hostedZone = HostedZone.fromLookup(this, 'hosted-zone', {
-      domainName
-    })
-
-
-
-    const certificate = new DnsValidatedCertificate(this, 'website-certificate', {
-      region: 'us-east-1',
-      hostedZone,
-      domainName
-    })
 
     const distribution = new Distribution(this, 'cloudfront-distribution', {
       domainNames: [domainName],
@@ -91,28 +116,29 @@ export class AppStack extends Stack {
       defaultBehavior: {
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         origin: new S3Origin(assetsBucket),
-      }
-    })
+      },
+    });
 
     new ARecord(this, 'a-record', {
       zone: hostedZone,
-      target: RecordTarget.fromAlias(
-        new CloudFrontTarget(distribution)
-      )
-    })
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    });
 
     const backendConfig: BackendConfig = {
       userpoolId: pool.userPoolId,
-      userPoolClientId: client.userPoolClientId
-    }
+      userPoolClientId: client.userPoolClientId,
+      authSignInUrl: userPoolDomain.signInUrl(client, {
+        redirectUri: `https://${domainName}`,
+      }),
+    };
 
     new BucketDeployment(this, 'deployment', {
       destinationBucket: assetsBucket,
       sources: [
         Source.asset(DEPLOYMENT_DIR),
-        Source.jsonData('backend-config.json', backendConfig)
+        Source.jsonData('backend-config.json', backendConfig),
       ],
-      distribution
+      distribution,
     });
   }
 }
